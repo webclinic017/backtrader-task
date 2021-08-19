51e394911c29608bf47061de2151d46921bd63d7
ajmalshukoor <ajmalshukoor11@gmail.com>
2021-08-18 22:44:57 -0400

    first commit

---
+++ signals.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+import backtrader as bt
+import yfinance as yf
+# Create a subclass of SignaStrategy to define the indicators and signals
+
+class SmaCross(bt.SignalStrategy):
+    # list of parameters which are configurable for the strategy
+    params = dict(
+        pfast=10,  # period for the fast moving average
+        pslow=30   # period for the slow moving average
+    )
+
+    def __init__(self):
+        sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
+        sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
+        crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
+        self.signal_add(bt.SIGNAL_LONG, crossover)  # use it as LONG signal
+
+
+cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
+
+# Create a data feed
+# data = bt.feeds.YahooFinanceData(dataname='AAPL',
+#                                  fromdate=datetime(2011, 1, 1),
+#                                  todate=datetime(2012, 12, 31),
+#                                  version='')
+
+data = bt.feeds.PandasData(dataname=yf.download('SPY', datetime(2011, 1, 1), datetime(2012, 12, 31)))
+
+
+cerebro.adddata(data)  # Add the data feed
+
+cerebro.addstrategy(SmaCross)  # Add the trading strategy
+cerebro.run()  # run it all
+cerebro.plot()  # and plot it with a single command
